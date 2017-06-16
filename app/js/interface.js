47c66b960557127334a466968ace36779e3498e0
Pavel-nineseven <pavel@nineseven.ru>
2017-06-16 14:38:01 +0300

    menu(not full), adaptive(not-full)

--- app/js/interface.js
+++ app/js/interface.js
@@ -1,8 +1,40 @@
 $(document).ready(function() {
 	flexibility(document.documentElement);
-	// $("body").on("click", ".test", function(e){
-	// 	e.preventDefault();
-	// })
+
+
+	//TOP-MENU
+	menu = $(".top-nav__list");
+	indicator = $('<span class="indicator"></span>');
+	menu.append(indicator);
+	position_indicator(menu.find(".top-nav__item.active"));  
+	setTimeout(function(){indicator.css("opacity", 1);}, 500);
+	// menu.find(".top-nav__item").mouseenter(function(){
+	// 	position_indicator($(this));
+	// });
+	// menu.find(".top-nav__item").mouseleave(function(){
+	// 	position_indicator(menu.find(".top-nav__item.active"));
+	// });
+
+	
+	//SUBNAV
+	$("body").on("click", ".top-nav__link", function(e){
+		e.preventDefault();
+		$('.top-nav__item').removeClass('active');
+		$(this).parents('.top-nav__item').addClass('active');
+		position_indicator(menu.find(".top-nav__item.active"));
+		$('.subnav').hide().removeClass('active'); 
+		$(this).next('.subnav').show().addClass('active');  
+	})
+
+
+
+	//SUBNAV-CLOSE
+	$("body").on("click", ".subnav__close", function(e){
+		e.preventDefault();
+		$(this).parents('.subnav').hide(); 
+	})
+
+	//SUBNAV
 
 });
 
@@ -10,7 +42,7 @@ $(document).ready(function() {
 
 
 $(window).resize(function () {
-
+	position_indicator(menu.find(".top-nav__item.active")); 
 });
 
 // $(window).load(function(){
@@ -18,7 +50,14 @@ $(window).resize(function () {
 // });
 
 // functions
-
+function position_indicator(ele){
+	var left = ele.offset().left;
+	var width = ele.width();
+	indicator.stop().animate({
+		left: left,
+		width: width
+	});
+}
 
 // links pages
 // $('body').append(
